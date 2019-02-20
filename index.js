da139854192b32b83c18bbf5d63fe78f1a7b7602
Ken Lai <ken.lai@redso.com.hk>
2019-02-20 12:00:23 +0800

    Fix ios crash

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-import { DeviceEventEmitter, NativeModules } from 'react-native';
+import { DeviceEventEmitter, NativeModules, Platform } from 'react-native';
 
 const { RNSmsRetrieverModule } = NativeModules;
 const EVEN_LISTENER = 'me.furtado.smsretriever:SmsEvent';
 
-const SmsRetrieverModule = {
+const SmsRetrieverModule = Platform.OS == "ios" ? {
+  requestPhoneNumber: () => new Promise(),
+  startSmsRetriever: () => new Promise(),
+  addSmsListener: (callback) => DeviceEventEmitter.addListener(EVEN_LISTENER, callback),
+  removeSmsListener: () => DeviceEventEmitter.removeAllListeners(EVEN_LISTENER)
+} : {
   requestPhoneNumber: RNSmsRetrieverModule.requestPhoneNumber,
   startSmsRetriever: RNSmsRetrieverModule.startSmsRetriever,
   addSmsListener: (callback) => DeviceEventEmitter.addListener(EVEN_LISTENER, callback),
